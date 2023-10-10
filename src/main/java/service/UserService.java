6ab3d68d88629f88b995374d62903d42251fec16
Aryan Sanjeev <sanjeevaryan2011@gmail.com>
2023-10-10 20:11:05 +0530

    Initial commit

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,20 @@
+package service;
+import java.sql.SQLException;
+
+import dao.UserDAO;
+import model.User;
+public class UserService {
+    public static Integer saveUser(User user) {
+        try {
+            if(UserDAO.isExists(user.getEmail())) {
+                return 0;
+            } else {
+                return UserDAO.saveUser(user);
+
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
